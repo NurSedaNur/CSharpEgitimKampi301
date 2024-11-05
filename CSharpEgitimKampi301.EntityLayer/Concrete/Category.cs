75ee866838f77af30b9eee41bcb979f6cf53c5eb
Seda Nur Ekici <sedanur.ekc.312@gmail.com>
2024-11-05 13:57:53 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
+
+//Field => int x; gibi değişken direkt sınıfın içinde tanımlanır, en üstte
+
+//Variable => metot içinde tanımlanan degişkenler
+
+//Property => degişken get, set alıyorsa denir
