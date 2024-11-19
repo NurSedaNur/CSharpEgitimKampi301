29e7ea0f0fee84357882eac20626da0461aab36e
Seda Nur Ekici <sedanur.ekc.312@gmail.com>
2024-11-20 01:13:46 +0300

    Data Access Katmanı, Context Sınıfı ve Migration İşlemleri yapıldı.

--- CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -11,11 +11,7 @@ namespace CSharpEgitimKampi301.EntityLayer.Concrete
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
         public bool CategoryStatus { get; set; }
+        public List<Product> Products { get; set; }
     }
 }
 
-//Field => int x; gibi değişken direkt sınıfın içinde tanımlanır, en üstte
-
-//Variable => metot içinde tanımlanan degişkenler
-
-//Property => degişken get, set alıyorsa denir
--- CSharpEgitimKampi301.EntityLayer/Concrete/Order.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Order.cs
@@ -6,9 +6,15 @@ using System.Threading.Tasks;
 
 namespace CSharpEgitimKampi301.EntityLayer.Concrete
 {
-    public class Order
+    public class Order//SİPARİŞ TABLOSU
     {
-        public int PropId { get; set; }
-
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
     }
 }
